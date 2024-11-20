69bbdc0c3603503901a1288441e6631a7dab8bf8
Faten Eid <fatenfarid1221@gmail.com>
2024-11-20 14:32:45 +0200

    solving from a to d

---
+++ training-sheets/assiut-sheet/sheet-01/b_basic_data_types.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    
+    //int, long long, char, float and double
+    
+    int i;
+    long long l;
+    char ch;
+    float f;
+    double d;
+    
+    cin>> i>>l>>ch>>f>>d;
+    
+    cout<<i<<'\n'<<l<<'\n'<<ch<<'\n'<<f<<'\n'<<d;
+    
+    
+}
