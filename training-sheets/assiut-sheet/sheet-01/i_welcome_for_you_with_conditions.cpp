27195d7d3251a16ff7222535dceb6ba90924cc0b
Faten Eid <fatenfarid1221@gmail.com>
2024-11-22 21:34:21 +0200

    solve problem I.

---
+++ training-sheets/assiut-sheet/sheet-01/i_welcome_for_you_with_conditions.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    double A, B;
+    cin >> A >> B;
+
+    if (A >= B) {
+        cout << "Yes";
+    }
+    else {
+        cout << "No";
+    }
+
+}
