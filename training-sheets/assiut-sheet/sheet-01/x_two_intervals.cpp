526cd3230bb2064cc05cff785addaa4c1e8aecd7
Faten Eid <87611899+fateneid@users.noreply.github.com>
2024-12-03 02:22:57 +0200

    x

---
+++ training-sheets/assiut-sheet/sheet-01/x_two_intervals.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    
+    int l1, r1, l2, r2;
+    cin >> l1 >> r1 >> l2 >> r2;
+    
+    if(r1 < l2 || r2 < l1){
+        cout << -1;
+    }
+    else{
+        cout << max(l1,l2) << " " << min(r1,r2);
+    }
+    
+    return 0; 
+
+}
