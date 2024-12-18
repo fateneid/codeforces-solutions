ef62b2f3a4961395b8db4b235d35969004e39ad4
Faten Eid <fatenfarid1221@gmail.com>
2024-12-18 23:54:49 +0200

    t

---
+++ training-sheets/assiut-sheet/sheet-02/t_shape2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	short N;
+	cin >> N;
+
+	for(short i = 1; i <= N; i++) {
+		for(short j = 1; j <= N - i; j++) {
+			cout << ' ';
+		}
+		for(short k = 1; k <= 2*i - 1; k++) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
