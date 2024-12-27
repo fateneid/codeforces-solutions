ffe200f69c6a1546c72846c12d118d7cb1d21afe
Faten Eid <fatenfarid1221@gmail.com>
2024-12-27 23:18:01 +0200

    Add solution for problem Z. Three Numbers from Sheet2 (Assiut Sheet)

---
+++ training-sheets/assiut-sheet/sheet-02/z_three_numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int k, s, count = 0;
+	cin >> k >> s;
+	
+	for(int x = 0; x <= k; x++) {
+		for(int y = 0; y <= k; y++) {
+			int z = s - x - y;
+			if(z >= 0 && z <= k) {
+				count++;
+			}
+
+		}
+	}
+
+	cout << count;
+
+	return 0;
+}
