e395831d92dcd7306cbf056fab1dc599affa385b
Faten Eid <fatenfarid1221@gmail.com>
2025-01-14 23:57:29 +0200

    cp: Solve Assiut Sheet / Sheet 4 / A. Create A New String

---
+++ training-sheets/assiut-sheet/sheet-04/a_create_a_new_string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s, t;
+    cin >> s;
+    cin >> t;
+    
+    cout << s.length() << ' ' << t.length() << '\n';
+    cout << s << ' ' << t;
+
+
+	return 0;
+}
