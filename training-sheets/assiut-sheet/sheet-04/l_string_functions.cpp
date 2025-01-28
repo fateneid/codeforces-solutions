44b14ff174deb09aa7828dc5f909c730aef9c2fc
Faten Eid <87611899+fateneid@users.noreply.github.com>
2025-01-28 11:49:26 +0200

    Solve: Assiut/S4/L - String Functions

--- training-sheets/assiut-sheet/sheet-04/l_string_functions.cpp
+++ training-sheets/assiut-sheet/sheet-04/l_string_functions.cpp
@@ -2,42 +2,41 @@
 #include <algorithm>
 #include <string>
 using namespace std;
-
 int main()
 {
 	int n, q;
 	cin >> n >> q;
 	string s;
 	cin >> s;
-
 	while(q--) {
 		string command;
 		cin >> command;
-		int start, end;
-		char ch;
+		int l, r;
 
 		if(command == "substr") {
-			cin >> start >> end;
-			cout << s.substr(start - 1, end - start + 1) << '\n';
+			cin >> l >> r;
+			int m = min(l, r);
+			cout << s.substr(m - 1, max(l, r) - m + 1) << '\n';
 		}
 		else if(command == "sort") {
-			cin >> start >> end;
-			sort(s.begin() + (start - 1), s.end() + end);
+			cin >> l >> r;
+			sort(s.begin() + min(l, r) - 1, s.begin() + max(l, r));
 		}
 		else if(command == "reverse") {
-			cin >> start >> end;
-			reverse(s.begin() + (start - 1), s.end() + end);
+			cin >> l >> r;
+			reverse(s.begin() + min(l, r) - 1, s.begin() + max(l, r));
 		}
 		else if(command == "pop_back") {
 			s.pop_back();
 		}
 		else if(command == "push_back") {
+			char ch;
 			cin >> ch;
 			s.push_back(ch);
 		}
 		else if(command == "print") {
-			cin >> start;
-			cout << s.at(start - 1) << '\n';
+			cin >> l;
+			cout << s[l - 1] << '\n';
 		}
 		else if(command == "front") {
 			cout << s.front() << '\n';
@@ -46,7 +45,6 @@ int main()
 			cout << s.back() << '\n';
 		}
 	}
-
 
 	return 0;
 }
