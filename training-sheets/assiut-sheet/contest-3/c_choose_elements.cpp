a4d2ddafe1bb961ebb560f3630265f46c0f1e99d
Faten Eid <mulan.tech.914h@gmail.com>
2025-02-16 12:53:31 +0200

    Fix: Assiut/C3/C - Choose Elements

--- training-sheets/assiut-sheet/contest-3/c_choose_elements.cpp
+++ training-sheets/assiut-sheet/contest-3/c_choose_elements.cpp
@@ -4,18 +4,23 @@ using namespace std;
 
 int main()
 {
-	int n, a[1001], k;
+	int n, k, a[1001];
 	cin >> n >> k;
 
 	for(int i = 0; i < n; i++) {
 		cin >> a[i];
 	}
 
-	sort(a, a + n);
+	sort(a, a + n, greater<int>());
 
 	long long sum = 0;
-	for(int i = n - 1; i >= n - k; i--) {
-		sum += a[i];
+	for(int i = 0; i < k; i++) {
+		if(a[i] > 0) {
+			sum += a[i];
+		}
+		else {
+			break;
+		}
 	}
 
 	cout << sum;
