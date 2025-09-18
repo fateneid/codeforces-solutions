1d6ee08a6bacf820dc145b63a45041aa8255e88f
Faten Eid <87611899+fateneid@users.noreply.github.com>
2025-09-19 00:00:17 +0300

    Solve: Assiut/S4/U - New Words

---
+++ training-sheets/assiut-sheet/sheet-04/u_new_words.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+	ios::sync_with_stdio(0); cin.tie(0);
+
+	string s;
+	cin >> s;
+
+	int f[5] = {0}; // EGYPT
+	for (char c : s) {
+		switch (toupper(c)) {
+		case 'E': f[0]++; break;
+		case 'G': f[1]++; break;
+		case 'Y': f[2]++; break;
+		case 'P': f[3]++; break;
+		case 'T': f[4]++; break;
+		}
+	}
+
+	cout << *min_element(f, f + 5);
+
+	return 0;
+}
