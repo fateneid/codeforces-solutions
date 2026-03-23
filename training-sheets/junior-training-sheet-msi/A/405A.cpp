6e1cfc05a2bbe57dfb5e294015efe0185dd0d153
Faten Eid <87611899+fateneid@users.noreply.github.com>
2026-03-23 13:26:40 +0200

    cf: 405A

---
+++ training-sheets/junior-training-sheet-msi/A/405A.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, a[100];
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) cin >> a[i];
+
+	sort(a , a + n);
+
+	for (int i = 0; i < n; i++) cout << a[i] << " ";
+
+	return 0;
+}
