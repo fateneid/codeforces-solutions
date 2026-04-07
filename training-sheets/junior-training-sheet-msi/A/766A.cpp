116fa21eb5053edcee7ae5ea50386e69384b9be3
Faten Eid <87611899+fateneid@users.noreply.github.com>
2026-04-07 23:22:27 +0200

    cf: 766A

---
+++ training-sheets/junior-training-sheet-msi/A/766A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	string a, b;
+	cin >> a >> b;
+
+	if (a == b) cout << -1;
+	else cout << max(a.size(), b.size());
+
+	return 0;
+
+}
